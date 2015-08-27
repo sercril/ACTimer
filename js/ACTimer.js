bd0ac25ee096adcb3299b3d91b06af97d73f0b2d
August <august@firefly.cc>
2015-08-26 22:33:14 -0500

    setting up structure

---
+++ js/ACTimer.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sercril on 8/26/15.
+ */
+("bootstrap");
+//# sourceMappingURL=ACTimer.js.map
---
+++ ts/ACTimer.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by sercril on 8/26/15.
+ */
+
+
+import bootstrap = module("bootstrap");
